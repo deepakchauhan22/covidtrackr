38d290f23d81e93afb18de89958dcdd24c884296
coritoindia <coritoindia@gmail.com>
2021-09-26 02:11:22 +0530

    Updates

---
+++ precache-manifest.b644d86a74ab3a9f60c73c609d756ff7.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc53cc79ba3ebad2a13a28aa68080ed2",
+    "url": "/covidtrackr/index.html"
+  },
+  {
+    "revision": "816060b3c6e1302231a6",
+    "url": "/covidtrackr/static/css/2.8a641599.chunk.css"
+  },
+  {
+    "revision": "c58a1ca928cb6d3b3c37",
+    "url": "/covidtrackr/static/css/main.523107c2.chunk.css"
+  },
+  {
+    "revision": "816060b3c6e1302231a6",
+    "url": "/covidtrackr/static/js/2.1770e446.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/covidtrackr/static/js/2.1770e446.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c58a1ca928cb6d3b3c37",
+    "url": "/covidtrackr/static/js/main.e9c94564.chunk.js"
+  },
+  {
+    "revision": "34d5fbfba4e5057828ec",
+    "url": "/covidtrackr/static/js/runtime-main.60cebe25.js"
+  },
+  {
+    "revision": "5eab890242e8b811865e1bd3a7fd7868",
+    "url": "/covidtrackr/static/media/FAQ.5eab8902.pdf"
+  },
+  {
+    "revision": "42949f4421195377af5a4373fd2b9162",
+    "url": "/covidtrackr/static/media/cough.42949f44.svg"
+  },
+  {
+    "revision": "e16a155234d70cdc6a8648c25750632a",
+    "url": "/covidtrackr/static/media/covid.e16a1552.svg"
+  },
+  {
+    "revision": "96d04938c5b366419038dd128093c3c9",
+    "url": "/covidtrackr/static/media/covidactive.96d04938.svg"
+  },
+  {
+    "revision": "742a04b3251a1f6627e4e17e36dc524a",
+    "url": "/covidtrackr/static/media/coviddeath.742a04b3.svg"
+  },
+  {
+    "revision": "13589c7e6721d83fab615bc642fce352",
+    "url": "/covidtrackr/static/media/covidrecover.13589c7e.svg"
+  },
+  {
+    "revision": "4c6822d3e554ac04cdea40319ac4583a",
+    "url": "/covidtrackr/static/media/face-mask.4c6822d3.svg"
+  },
+  {
+    "revision": "ffe8659e9f90303c5636c00e23c5440d",
+    "url": "/covidtrackr/static/media/fever.ffe8659e.svg"
+  },
+  {
+    "revision": "5d21c6d0401341c772fe520d25cdf944",
+    "url": "/covidtrackr/static/media/fight.5d21c6d0.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/covidtrackr/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/covidtrackr/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/covidtrackr/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/covidtrackr/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/covidtrackr/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "13032f25b02f57a613f6d2748e28726c",
+    "url": "/covidtrackr/static/media/hero.13032f25.png"
+  },
+  {
+    "revision": "745c5655c7260608903308efa70186d7",
+    "url": "/covidtrackr/static/media/pandemic.745c5655.png"
+  },
+  {
+    "revision": "586260e7bf5b7c2676ecba93dfd9dc85",
+    "url": "/covidtrackr/static/media/protection.586260e7.png"
+  },
+  {
+    "revision": "052768334c109d8a67b59b495509219c",
+    "url": "/covidtrackr/static/media/socialdist.05276833.svg"
+  },
+  {
+    "revision": "358b15f9753556213fd817942c85ef64",
+    "url": "/covidtrackr/static/media/stay-safe.358b15f9.png"
+  },
+  {
+    "revision": "a319194d44d229465d1e5580d7bd168a",
+    "url": "/covidtrackr/static/media/wash-hands.a319194d.svg"
+  }
+]);
